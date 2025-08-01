7df789f436e73a07d7c7f8e5025568842d4d7e8d
Joshua K John <joshuakjohn7@gmail.com>
2025-08-01 23:13:41 +0530

    added skills

--- src/app/components/skill-icons/skill-icons.component.ts
+++ src/app/components/skill-icons/skill-icons.component.ts
@@ -3,14 +3,24 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import {
   ANGULAR,
+  CSS,
+  DOCKER,
   EXPRESS,
+  GIT,
   GITHUB,
+  HTML,
   INSTAGRAM,
+  JAVA,
   JAVASCRIPT,
   LINKEDIN,
   MONGODB,
+  MS,
   NODE,
+  PYTHON,
+  RESTAPI,
   RIGHT_ARROW,
+  SPRING,
+  SQL,
   TYPESCRIPT
 } from 'src/assets/svg-icons';
 
@@ -20,19 +30,29 @@ import {
   styleUrls: ['./skill-icons.component.scss']
 })
 export class SkillIconsComponent {
-  icons = ['Angular', 'javascript', 'typescript', 'node', 'mongodb', 'typescript', 'node', 'Mongo DB']; // Add more as needed
+  icons = ['Angular', 'Javascript', 'Typescript', 'Node.js', 'MongoDB', 'Express', 'Java', 'Spring Boot', 'HTML', 'CSS', 'SQL', 'REST API', 'Microservices', 'Git', 'Docker', 'Python']; // Add more as needed
   rotationStates: boolean[] = [];
   popInDelays: string[] = []; // 💡 Store animation delays as strings like '2.4s'
 
   constructor(private sanitizer: DomSanitizer, private iconRegistry: MatIconRegistry) {
     // SVG icon registration (same as before)...
     iconRegistry.addSvgIconLiteral('right-arrow', sanitizer.bypassSecurityTrustHtml(RIGHT_ARROW));
+    iconRegistry.addSvgIconLiteral('Docker', sanitizer.bypassSecurityTrustHtml(DOCKER));
+    iconRegistry.addSvgIconLiteral('Python', sanitizer.bypassSecurityTrustHtml(PYTHON));
+    iconRegistry.addSvgIconLiteral('Git', sanitizer.bypassSecurityTrustHtml(GIT));
+    iconRegistry.addSvgIconLiteral('Microservices', sanitizer.bypassSecurityTrustHtml(MS));
+    iconRegistry.addSvgIconLiteral('Spring Boot', sanitizer.bypassSecurityTrustHtml(SPRING));
+    iconRegistry.addSvgIconLiteral('REST API', sanitizer.bypassSecurityTrustHtml(RESTAPI));
+    iconRegistry.addSvgIconLiteral('SQL', sanitizer.bypassSecurityTrustHtml(SQL));
+    iconRegistry.addSvgIconLiteral('CSS', sanitizer.bypassSecurityTrustHtml(CSS));
+    iconRegistry.addSvgIconLiteral('HTML', sanitizer.bypassSecurityTrustHtml(HTML));
+    iconRegistry.addSvgIconLiteral('Java', sanitizer.bypassSecurityTrustHtml(JAVA)),
     iconRegistry.addSvgIconLiteral('Javascript', sanitizer.bypassSecurityTrustHtml(JAVASCRIPT)),
     iconRegistry.addSvgIconLiteral('Typescript', sanitizer.bypassSecurityTrustHtml(TYPESCRIPT)),
     iconRegistry.addSvgIconLiteral('Angular', sanitizer.bypassSecurityTrustHtml(ANGULAR)),
-    iconRegistry.addSvgIconLiteral('Mongo DB', sanitizer.bypassSecurityTrustHtml(MONGODB)),
-    iconRegistry.addSvgIconLiteral('express', sanitizer.bypassSecurityTrustHtml(EXPRESS)),
-    iconRegistry.addSvgIconLiteral('Node', sanitizer.bypassSecurityTrustHtml(NODE)),
+    iconRegistry.addSvgIconLiteral('MongoDB', sanitizer.bypassSecurityTrustHtml(MONGODB)),
+    iconRegistry.addSvgIconLiteral('Express', sanitizer.bypassSecurityTrustHtml(EXPRESS)),
+    iconRegistry.addSvgIconLiteral('Node.js', sanitizer.bypassSecurityTrustHtml(NODE)),
     iconRegistry.addSvgIconLiteral('github', sanitizer.bypassSecurityTrustHtml(GITHUB)),
     iconRegistry.addSvgIconLiteral('linkedin', sanitizer.bypassSecurityTrustHtml(LINKEDIN)),
     iconRegistry.addSvgIconLiteral('instagram', sanitizer.bypassSecurityTrustHtml(INSTAGRAM))
